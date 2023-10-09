407ae759d420baf0292a543800fb82071077c8ba
tchatzian <75744098+tchatzian@users.noreply.github.com>
2023-10-09 03:19:49 +0300

    Update mining.py

--- flasktemp/mining.py
+++ flasktemp/mining.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 import hashlib
 
-
 def get_sha_256_hash(input_value):
     return hashlib.sha256(input_value).hexdigest()
 
-
 def block_hash_less_than_target(block_hash, given_target):
     return int(block_hash, 16) < int(given_target, 16)
-
 
 # Initial block data (the transactions' merkle tree root, timestamp, client version, hash of the previous block)
 blockData = \
@@ -21,7 +18,6 @@ blockData = \
     '34104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649' \
     'f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000' \
         .encode()
-
 # Initial target - this is the easiest it will ever be to mine a Bitcoin block
 target = '0x00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
 
@@ -37,10 +33,8 @@ while not solution_found:
     second_hash = get_sha_256_hash(first_hash.encode())
 
     #print('Nonce: ' + str(nonce))
-
     #print('Block hash:')
     #print(second_hash)
-
     #print('Is the block hash less than the target?')
     solution_found = block_hash_less_than_target(second_hash, target)
     #print(solution_found)
